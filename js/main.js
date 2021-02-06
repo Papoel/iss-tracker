b5b5faeb10c5d4d12fa4b2c8e613bdd5fafab2d1
Papoel <pascal.briffard@gmail.com>
2021-02-06 19:26:25 +0100

    Intégration de la mapMonde de l'icone satelite et du suivi par géolocalisiation

--- js/main.js
+++ js/main.js
@@ -1,22 +1,67 @@
-const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
-getISS()
-// Récupérer les données d'un API - Création d'un fonction assychrone
-// Utlisation de fetch
+/*
+* async encapsule le résultat de la fonction
+* dans une promesse
+* async permet l'utilisation du mot clef
+* await dans le corps de la fonction
+* Le mot clef await, placé devant une promesse
+* permet de mettre en pause l'exécution du code :
 
+* await bloque l'exécution du code tant
+* que la promesse n'est pas terminée
+* await fonctionne avec les promesses
+* mais pas avec les callbacks
+* await ne peut être utilisé que
+* dans une fonction asynchrone
+*/
+
+/*
+ #1 Récupérer les données d'un API -
+ #2 Création d'un fonction assychrone
+ #3 Utlisation de fetch
+ #4 Destructuring pour récupérer dans une variable les données qui m'interesse
+ #5 Géolocaliser et placer un repere sur une map
+*/
+//?1 :
+
+// --------------- PARTIE MAP --------------------
+
+// - Création de la map et tuiles
+const mymap = L.map('issMap').setView([0, 0], 1)
+const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+const tileUrl = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+const tiles = L.tileLayer(tileUrl, { attribution })
+tiles.addTo(mymap)
+
+//- Prametrer l'icone et la personaliser
+  const issIcon = L.icon({
+    iconUrl: '../images/International_Space_Station.svg',
+    iconSize: [50, 32],
+    iconAnchor: [25, 16],
+   });
+
+const marker = L.marker([0, 0],{icon: issIcon}).addTo(mymap)
+
+// --------------- PARTIE API --------------------
+const api_url = 'https://api.wheretheiss.at/v1/satellites/25544'
+
+getISS()
+
+//?2 :
 async function getISS() {
   const response = await fetch(api_url)
   const data = await response.json()
-  // console.log(data.latitude)
-  // console.log(data.longitude)
+
   //+ Destructurer :
-  const { latitude, longitude, velocity } = data
+  const {
+    latitude,
+    longitude,
+  } = data
+
+  // L.marker([latitude, longitude]).addTo(mymap)
+  marker.setLatLng([latitude, longitude])
 
   //? Injecter les coordnnées dans la span
   document.getElementById('lat').innerHTML = (latitude)
   document.getElementById('lon').innerHTML = (longitude)
-  document.getElementById('vel').innerHTML = (velocity)
 
-  console.log(longitude)
-  console.log(latitude)
-  console.log(data)
 }
